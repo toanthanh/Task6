3f637ecd9510a65aea094332374d08c405c2fc42
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2017-11-17 01:24:38 +0200

    update

---
+++ js/main-b.js
@@ -0,0 +1,39 @@
+'use strict';
+// Create function 'showImages' which
+// loads images.json which has links to images as an array.
+
+// create a loop which builds the following HTML from every image in the array:
+/*
+ <li>
+ <figure>
+ <a href="img/original/filename.jpg"><img src="img/thumbs/filename.jpg"></a>
+ <figcaption>
+ <h3>Title</h3>
+ </figcaption>
+ </figure>
+ </li>
+ */
+// After the loop print the HTML into <ul> element using innerHTML.
+const showImages = () => {
+  const ul = document.querySelector('ul');
+
+  fetch('images.json').then((response) => {
+    return response.json();
+  }).then((json) => {
+    let html = '';
+    json.forEach((image) => {
+      html +=
+          `<li>
+          <figure>
+            <a href="img/original/${image.mediaUrl}"><img src="img/thumbs/${image.mediaThumb}"></a>
+            <figcaption>
+                <h3>${image.mediaTitle}</h3>
+            </figcaption>
+          </figure>
+        </li>`;
+    });
+    ul.innerHTML = html;
+  });
+};
+
+showImages();
